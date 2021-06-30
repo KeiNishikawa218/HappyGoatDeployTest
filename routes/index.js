e25331f2af9f680dc023779b3057d7348f455829
KeiNishikawa218 <k-nishikawa@atware.co.jp>
2021-06-30 18:20:35 +0900

    手紙を書くページと表示するページを追加

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/writeLetter', function(req, res, next) {
+  res.render('writeLetter', { title: 'writeLetter' });
+});
+
+router.get('/showLetter', function(req, res, next) {
+  res.render('showLetter');
+});
+
 module.exports = router;
