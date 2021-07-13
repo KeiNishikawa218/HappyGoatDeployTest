36bd781da014b18c954a7a826557f3fd96d26723
KeiNishikawa218 <k-nishikawa@atware.co.jp>
2021-07-13 18:56:08 +0900

    indexのテストを追加. yarn mochaでテストが走る用に設定

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,20 @@
-var request = require('supertest')
-    , app = require('../app');
+const request = require('supertest');
+const app = require('../app');
 
 // TODO:indexをテストする場合はプログラムが終わらない
 // SQLが原因だと思うので調べる
-
-// describe("GET /", function () {
-//     it("welcomes the user", function (done) {
-//         request(app).get("/")
-//             .expect(200, done)
-//     })
-// })
+describe("GET /", function () {   
+    it("welcomes the user", function (done) {
+        request(app)
+            .get("/")
+            .expect(200, done);
+    });
+});
 
 describe("GET /new", function () {
-    it("welcomes the user", function (done) {
-        request(app).get("/new")
-            .expect(200, done)
-    })
-
-})
+    it("you can write a letter", function (done) {
+        request(app)
+            .get("/new")
+            .expect(200, done);
+    });
+});
