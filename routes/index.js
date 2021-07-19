34f46da50b8c2232dd018ae89740ea8d2fb5b297
kazuhitonakayama <firstjin19990101@gmail.com>
2021-07-19 13:21:12 +0900

    Refactoring

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const sql_connection = require('../lib/mysql_module.js');
+const letters = require('../lib/letters.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  sql_connection.find_letters()
-    .then(letters => {
-      res.render('index', { title: 'index', letters: letters }); 
+  letters.findAll().then(results=> {
+      res.render('index', { title: 'index', letters: results }); 
     }
   );
 });
 
-// 新規投稿
+// 新規投稿画面の表示
 router.get('/letters/new', function(req, res, next) {
   res.render('new', { title: 'ハッピーゴート | 文章の入力' });
 });
 
+// 新規投稿機能
 router.post('/letters/new', function(req, res, next) {
-  // console.log(req.body.sentence);
-  const subject = req.body.subject;
-  const body = req.body.body;
-  const post_data = { id: null, subject: subject, body: body };
+  const post_data = { id: null, subject: req.body.subject, body: req.body.body };
 
-  sql_connection.connection.query('INSERT INTO letters SET ?', post_data, function (results, fields) {
-      // if (error) throw error;
-      console.log(post_data['subject'] + "を投稿したよ！");
-      // console.log('ID:', results.insertId);
-  });
-
-  res.redirect("/");
+  letters.newLetter(post_data).then(data => {
+      res.redirect("/")
+    }
+  );
 });
 
 // 詳細画面
 router.get('/letters/:id(\\d+)', function (req, res, next) {
-  sql_connection.sql_statment('select * FROM letters where id = ' + req.params.id,(error, results) => {
-    res.render('show', {title: results[0].subject,letter: results} );
-  });
+  letters.findById(req.params.id).then(results=> {
+      res.render('show', { title: results[0].subject, letter: results[0] }); 
+    }
+  );
 });
 
 module.exports = router;
