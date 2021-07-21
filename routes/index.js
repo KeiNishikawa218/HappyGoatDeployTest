c3cc4b34037c9599d7bb2ff7752649d720786bd8
kazuhitonakayama <firstjin19990101@gmail.com>
2021-07-21 14:09:02 +0900

    enable to fetch image from S3

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = express.Router();
 const letters = require('../lib/letters.js');
 const ogpGenerator = require('../lib/ogpGenerator');
 const uploadImageToS3 = require('../lib/uploadImageToS3');
+const downloadImageFromS3 = require('../lib/downloadImageFromS3');
 
 
 /* GET home page. */
@@ -20,17 +23,23 @@ router.get('/letters/new', function(req, res, next) {
 
 // 新規投稿機能
 router.post('/letters/new', function(req, res, next) {
-  const post_data = { id: null, subject: req.body.subject, body: req.body.body };
-
+  // 画像の生成　返り値で'画像のDataURI'が帰ってくる
   const image = ogpGenerator.generate(
-    post_data.subject,
-    post_data.body
+    req.body.subject,
+    req.body.body
   );
 
-  uploadImageToS3.uploadFile(image);
+  // Data URI形式で画像をS3にupload
+  uploadImageToS3.uploadFile(image).then(
+    response => {
+      // S3にアップして生成されたURLを`uploadedImage`に格納
+      const uploadedImage = process.env.BUCKET_URL + downloadImageFromS3.downloadFile(response);
+      // S3にアップロードされた画像をimageパラメータに付与
+      const post_data = { id: null, subject: req.body.subject, body: req.body.body,image: uploadedImage };
 
-  letters.createLetter(post_data).then(data => {
-      res.redirect("/")
+      letters.createLetter(post_data).then(
+          res.redirect("/")
+      );
     }
   );
 });
