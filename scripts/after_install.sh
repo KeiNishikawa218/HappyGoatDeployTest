b73750d93915e61f5cfaecd638fbb16c777e559d
KeiNishikawa218 <k-nishikawa@atware.co.jp>
2021-07-26 21:51:07 +0900

    install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-source /home/ec2-user/.nvm/nvm.sh
-
-# mkdir /home/ec2-user/happygoat
 cd /home/ec2-user/happygoat
 
 nvm use node
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,14 +3,12 @@
 # Before deploy, make sure EC2 has CodeDeploy Agent
 
 # Take off commentout when you deploy to new EC2 instance
-# Install node.js, yarn, nvm, forever
+# Install nvm, yarn, and forever
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 source root/.nvm/nvm.sh
 nvm install node
-
-# Install forever module
-# https://www.npmjs.com/package/forever
-# sudo npm install forever -g
+sudo npm install -g yarn
+sudo yarn add forever
 
 # Clean working folder
 # sudo find /home/ec2-user/happygoat -type f -delete
