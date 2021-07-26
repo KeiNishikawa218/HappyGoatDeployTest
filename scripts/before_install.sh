d94f426acedbfe919f52af8b10492c1aa3eabd3d
KeiNishikawa218 <k-nishikawa@atware.co.jp>
2021-07-26 21:04:50 +0900

    back

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,7 +8,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 
-
 # Install forever module
 # https://www.npmjs.com/package/forever
 # sudo npm install forever -g
