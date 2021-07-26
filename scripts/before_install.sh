5162127e4c979ef52d1276d2fc1554e9f79cfd02
KeiNishikawa218 <k-nishikawa@atware.co.jp>
2021-07-26 21:38:06 +0900

    test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,7 +5,7 @@
 # Take off commentout when you deploy to new EC2 instance
 # Install node.js, yarn, nvm, forever
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-touch root/.nvm/nvm.sh
+. ~/.nvm/nvm.sh
 nvm install node
 
 # Install forever module
